8eb6fc87e50cd28cda17d4b473be2f2c15b83098
Cian1809 <cianreevesoshea@gmail.com>
2024-10-18 06:47:24 +0100

    Challenge 2 B00164770
    Unit 2 Challenge 2 Interactive Multimedia

---
+++ Assets/Challenge 2/Scripts/DestroyOutOfBoundsX.cs
@@ -0,0 +1,26 @@
+﻿//B00164770
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOutOfBoundsX : MonoBehaviour
+{
+    private float leftLimit = -30;//changed to negative
+    private float bottomLimit = -5;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Destroy dogs if x position less than left limit
+        if (transform.position.x < leftLimit)//swapped sign
+        {
+            Destroy(gameObject);
+        } 
+        // Destroy balls if y position is less than bottomLimit
+        else if (transform.position.y < bottomLimit)//Changed z to y
+        {
+            Destroy(gameObject);
+        }
+
+    }
+}
---
+++ Assets/Challenge 2/Scripts/SpawnManagerX.cs
@@ -0,0 +1,38 @@
+﻿//B00164770
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManagerX : MonoBehaviour
+{
+    public GameObject[] ballPrefabs;
+
+    private float spawnLimitXLeft = -22;
+    private float spawnLimitXRight = 7;
+    private float spawnPosY = 30;
+
+	
+    private float startDelay = 1.0f;
+    private float spawnInterval;
+
+    // Start is called before the first frame update
+    void Start()
+    {//Removed InvokeRepeating, caused the ball repeating at the same time
+        SpawnRandomBall();
+    }
+
+    // Spawn random ball at random x position at top of play area
+    void SpawnRandomBall ()
+    {
+        // Generate random ball index and random spawn position
+        Vector3 spawnPos = new Vector3(Random.Range(spawnLimitXLeft, spawnLimitXRight), spawnPosY, 0);
+
+        // instantiate ball at random spawn location
+		int ballColour = Random.Range(0, ballPrefabs.Length);//Random range for all ball colours, replacing the [0]
+        Instantiate(ballPrefabs[ballColour], spawnPos, ballPrefabs[ballColour].transform.rotation);
+		Invoke("SpawnRandomBall", Random.Range(1,5));
+		//Range 1 sec to 5 sec for the ball to spawn
+		
+    }
+
+}
